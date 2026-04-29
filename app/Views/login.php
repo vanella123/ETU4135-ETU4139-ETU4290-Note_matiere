09bf5091bccf46dbe94da5407a9816ccfdb93864
vaa <vaa@localhost>
2026-04-29 10:39:19 +0300

    page login

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>SysInfo — Connexion</title>
-  <link rel="stylesheet" href="<?= base_url('Design/style.css') ?>" />
-</head>
+  <link rel="stylesheet" href="<?= base_url('css/style.css') ?>" />
+</hea  d>
 <body>
 
 <div class="login-page">
